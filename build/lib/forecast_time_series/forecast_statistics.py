49c3ca11e3455c683b3f7b1e053fd1fb79717f2a
computergraphicsforall <computergraphicsforall@gmail.com>
2021-01-18 16:51:35 -0500

    update time_series results vs stkde

--- build/lib/forecast_time_series/forecast_statistics.py
+++ build/lib/forecast_time_series/forecast_statistics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import stats
+import matplotlib.pyplot as plt
 import math
 
 
@@ -57,3 +58,90 @@ def get_statistics(ffd, fip, ffp, fie, dpt, dpe, dt):
                        )
 
     return results
+
+
+def pai(fcast, percentage):
+    pai_data = np.sort(fcast.ravel())
+    step = math.floor((len(pai_data.ravel()) * percentage) / 100)
+
+    A = len(pai_data)
+    N = pai_data.sum()
+
+    pai_array = list()
+    area_per = list()
+    init_pai = True
+    a = 0
+    while init_pai:
+
+        a = a + step
+
+        if a < A:
+            n = pai_data[0:a].sum()
+            pai_box = ((n / N) * 100) / ((a / A) * 100)
+            pai_array.append(pai_box)
+            area_per.append((a / A) * 100)
+        else:
+            a = A
+            n = pai_data[0:].sum()
+            pai_box = ((n / N) * 100) / ((a / A) * 100)
+            init_pai = False
+            pai_array.append(pai_box)
+            area_per.append((a / A) * 100)
+
+    return pai_array, area_per
+
+
+def results_forecast_comparison(mesh_data, fcast_stkde, fcast_tseries, obs, bandwidth):
+    fcast_stkde = np.array(fcast_stkde)
+    fcast_tseries = np.array(fcast_tseries)
+    obs = np.array(obs)
+
+    # plot
+    fig1, ax1 = plt.subplots(1, 2, figsize=(25, 10))
+
+    ocp = ax1[0].pcolor(*mesh_data, fcast_stkde)
+    ts = ax1[1].pcolor(*mesh_data, fcast_tseries)
+
+    cb1 = plt.colorbar(ocp, ax=ax1[0])
+    cb2 = plt.colorbar(ts, ax=ax1[1])
+    cb1.set_label("Relative risk")
+    cb2.set_label("Incidents forecast")
+    ax1[0].set_title('Open cp STKDE forecast')
+    ax1[1].set_title('Time series forecast')
+
+    # normalized observations and forecast for open cp and time series
+    norm_matrix_obser = obs / obs.max()
+    norm_matrix_fcast_ocp = fcast_stkde / fcast_stkde.max()
+    norm_matrix_fcast_ts = fcast_tseries / fcast_tseries.max()
+
+    # difference bewtween methods forecasting
+    dif_ocp = norm_matrix_obser.ravel() - norm_matrix_fcast_ocp.ravel()
+    dif_ts = norm_matrix_obser.ravel() - norm_matrix_fcast_ts.ravel()
+    fig2 = plt.figure(figsize=(15, 5))
+    plt.title('Difference STKDE - Time series')
+    plt.xlabel('Box grid')
+    plt.ylabel('Difference')
+    plt.plot(dif_ocp, color='green', label='ST method')
+    plt.plot(dif_ts, color='red', label='ST KDE')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    pai_tseries, area_tseries = pai(fcast=norm_matrix_fcast_ts, percentage=10)
+    pai_stkde, area_stkde = pai(fcast=norm_matrix_fcast_ocp, percentage=10)
+
+    fig2 = plt.figure(figsize=(15, 5))
+    plt.plot(pai_stkde, area_stkde, label='STKDE PAI')
+    plt.plot(pai_tseries, area_tseries, label='Time series PAI')
+    plt.title('PAI STKDE - Time series')
+    plt.xlabel('Hit rate')
+    plt.ylabel('Area percentage')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    RMSE_TS = np.sqrt(
+        np.sum((norm_matrix_obser.ravel() - norm_matrix_fcast_ts.ravel()) ** 2) * (1 / len(norm_matrix_obser.ravel())))
+    RMSE_STKDE = np.sqrt(
+        np.sum((norm_matrix_obser.ravel() - norm_matrix_fcast_ocp.ravel()) ** 2) * (1 / len(norm_matrix_obser.ravel())))
+    print('RMSE_STKDE-%s: %f ' % (str(bandwidth), RMSE_STKDE), 'RMSE_TS-%s: %f ' % (str(bandwidth), RMSE_TS))
---
+++ tools/.ipynb_checkpoints/spatial_tools-checkpoint.py
@@ -0,0 +1,241 @@
+import math
+import numpy as np
+import plotly.graph_objects as go
+import json
+
+
+def bw_intervals(mts, long, lat):
+    """
+    Function to obtain the intervals in latitude and longitude for crime prediction
+
+    :param mts: bandwidth expected for intervals. This value correspond to the a circle diameter in meters units
+    :param long: longitude value of centroid
+    :param lat: latitude value of centroid
+    :return: two float arrays with the intervals in longitude and latitude for the bandwidth
+    """
+
+    """
+    Details: 
+        1 degree of latitude to equals 111.31 kms in average
+        1 degree of longitude depend of latitude value because earth shape
+        To calculate a proximal degrees in longitude that would to sum for the bandwidth, we need know the latitude value: 
+            1 degree of longitude correspond to  40075 kms * cos(latitude en radians) / 360
+    """
+    cearth = 40075  # earth circumference in kms
+    onedeg_km_lat = cearth / 360  # 1 degree to kms latitude
+    mt_km = mts / 1000  # mts to kms
+    rad = mt_km * 0.5  # radius for the bandwidth
+
+    onedeg_km_lon = cearth * (math.cos(math.radians(lat)) / 360)
+
+    lat_min = lat - (rad / onedeg_km_lat)
+    lat_max = lat + (rad / onedeg_km_lat)
+
+    long_min = long - (rad / onedeg_km_lon)
+    long_max = long + (rad / onedeg_km_lon)
+
+    return [long_min, long_max], [lat_min, lat_max]
+
+
+def distance_points(lat1, lon1, lat2, lon2):
+    """
+    Function to find the distance between two points in latitude and longitude values
+
+    :param lat1: latitude value of point 1
+    :param lon1: longitude value of point 1
+    :param lat2: latitude value of point 2
+    :param lon2: longitude value of point 2
+    :return: the distance in meters between the points
+    """
+
+    r = 6378.137  # Radius of earth in KM
+    dlat = lat2 * math.pi / 180 - lat1 * math.pi / 180
+    dlon = lon2 * math.pi / 180 - lon1 * math.pi / 180
+    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(lat1 * math.pi / 180) * math.cos(
+        lat2 * math.pi / 180) * math.sin(dlon / 2) * math.sin(dlon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = r * c
+    return d * 1000  # meters
+
+
+def get_degrees(mts, lat, t):
+    """
+    Function to convert meters in degrees
+
+    :param mts: value in meters to convert
+    :param lat: reference latitude value
+    :param t: parameter to specify whether the degrees are in longitude or latitude. get "lon" for longitude and "lat" for latitude
+    :return: degrees in latitude or longitude
+    """
+
+    cearth = 40075  # earth circumference in kms
+    onedeg_km_lat = cearth / 360  # 1 degree to kms latitude
+    mt_km = mts / 1000  # mts to kms
+
+    onedeg_km_lon = cearth * (math.cos(math.radians(lat)) / 360)
+    if t == 'lat':
+        deg = mt_km / onedeg_km_lat
+    if t == 'lon':
+        deg = mt_km / onedeg_km_lon
+    return deg
+
+
+def index_centroids_incidents_by_mesh(data, mesh, bandwidth):
+
+    x, y = mesh
+    title_x = 'CX_' + str(bandwidth)
+    title_y = 'CY_' + str(bandwidth)
+    pi_x = x[0]                      # starting point at x of the mesh
+    pi_y = y[0]                      # starting point at y of the mesh
+    dis_x = abs(x[0] - x[-1])        # maximum distance between start and end point in x
+    dis_y = abs(y[0] - y[-1])        # maximum distance between start and end point in y
+    max_index_x = len(x) - 1         # maximum centroid index in x
+    max_index_y = len(y) - 1         # maximum centroid index in y
+
+    data[title_x] = np.floor((abs(pi_x - data.LONGITUD) * max_index_x) / dis_x)
+    data[title_y] = np.floor((abs(pi_y - data.LATITUD) * max_index_y) / dis_y)
+    data[title_x] = data[title_x].astype(int)
+    data[title_y] = data[title_y].astype(int)
+
+    return data
+
+
+def grid_forecast(data, bandwidth=500, c_points=False, token_mapbox=None, figure=False):
+    x, y, xp, yp, lcl = None, None, None, None, None
+    # constants
+    add_mts = 2 / 111.32
+    min_lat = 4.38981655133336
+
+    max_lat = data['LATITUD'].max()
+    min_lon = data['LONGITUD'].min()
+    max_lon = data['LONGITUD'].max() + add_mts
+
+    # calculate the distance between the min and max
+    dis_lat = distance_points(min_lat, min_lon, max_lat, min_lon)
+    dis_lon = distance_points(min_lat, min_lon, min_lat, max_lon)
+
+    # calculate in degrees the distance between points
+    step_x = get_degrees(bandwidth, min_lat, 'lat')
+    step_y = get_degrees(bandwidth, min_lat, 'lon')
+
+    # generate the x and y values to the grid
+    x = np.arange(min_lon, max_lon, step_x)
+    y = np.arange(min_lat, max_lat, step_y)
+    x1, y1 = np.meshgrid(x, y)
+
+    if c_points:
+        min_xp = min_lon + (step_x / 2)
+        min_yp = min_lat + (step_y / 2)
+        max_xp = max_lon - (step_x / 2)
+        max_yp = max_lat - (step_x / 2)
+
+        xp = np.arange(min_xp, max_xp, step_x)
+        yp = np.arange(min_yp, max_yp, step_y)
+        x2, y2 = np.meshgrid(xp, yp)
+
+    if figure:
+        with open('../data/lcl.geojson') as f:
+            lcl = json.load(f)
+        if token_mapbox is None:
+            token_mapbox = 'pk.eyJ1IjoibWJhcnJlcm9wIiwiYSI6ImNrN28zZjczbTA0ZWwzaXF3aDAxcHl1dGkifQ.DlJqaPP1eC7PF_y-bbWjeg'
+        fig = go.Figure()
+        for j in range(len(y)):
+            fig.add_trace(go.Scattermapbox(mode="markers+lines", lon=x1[0],
+                                           lat=y1[j], line={'color': 'rgba(255, 255, 255, 0.71)'},
+                                           marker={'size': 10, 'color': 'rgba(203, 255, 92, 0.71)'},
+                                           showlegend=False))
+        for i in range(len(x)):
+            fig.add_trace(go.Scattermapbox(mode="markers+lines",
+                                           lon=x1.T[i], lat=y1.T[0],
+                                           line={'color': 'rgba(255, 255, 255, 0.71)'},
+                                           marker={'size': 10, 'color': 'rgba(203, 255, 92, 0.71)'},
+                                           showlegend=False))
+        if c_points:
+            for j in range(len(yp)):
+                fig.add_trace(go.Scattermapbox(mode="markers+lines",
+                                               lon=x2[0], lat=y2[j],
+                                               marker={'size': 10, 'color': 'red'},
+                                               showlegend=False))
+            for i in range(len(xp)):
+                fig.add_trace(go.Scattermapbox(mode="markers+lines",
+                                               lon=x2.T[i], lat=y2.T[0],
+                                               marker={'size': 10, 'color': 'red'},
+                                               showlegend=False))
+
+            fig.update_layout(mapbox={'style': "dark", 'accesstoken': token_mapbox,
+                                      'center': {'lon': -74.081749, 'lat': 4.6097102},
+                                      'zoom': 7, 'layers': [{'source': lcl, 'type': "fill", 'below': "traces",
+                                                             'color': "rgba(92, 214, 255, 0.5)"}]},
+                              margin={'l': 0, 'r': 0, 'b': 0, 't': 0})
+
+        fig.show()
+    return (x, y), (xp, yp)
+
+
+def grid_forecast_opencp(mesh_open_cp, bandwidth=500, c_points=False, token_mapbox=None, figure=False):
+    x, y, xp, yp, lcl = None, None, None, None, None
+    # constants
+    add_mts = 2 / 111.32
+    min_lat = mesh_open_cp[1].min()
+    max_lat = mesh_open_cp[1].max()
+    min_lon = mesh_open_cp[0].min()
+    max_lon = mesh_open_cp[0].max()
+
+    # calculate the distance between the min and max
+    dis_lat = distance_points(min_lat, min_lon, max_lat, min_lon)
+    dis_lon = distance_points(min_lat, min_lon, min_lat, max_lon)
+
+    # calculate in degrees the distance between points
+    step_x = get_degrees(bandwidth, min_lat, 'lat')
+    step_y = get_degrees(bandwidth, min_lat, 'lon')
+    # generate the x and y values to the grid
+    x = mesh_open_cp[0]
+    y = mesh_open_cp[1]
+    x1, y1 = np.meshgrid(mesh_open_cp[0], mesh_open_cp[1])
+
+    if c_points:
+        min_xp = min_lon + (step_y / 2)
+        min_yp = min_lat + (step_y / 2)
+        max_xp = max_lon + (step_y / 2)
+        max_yp = max_lat - (step_y / 2)
+        xp = np.arange(min_xp, max_xp, step_y)
+        yp = np.arange(min_yp, max_yp + (step_y / 2), step_y)
+        x2, y2 = np.meshgrid(xp, yp)
+
+    if figure:
+        with open('../data/lcl.geojson') as f:
+            lcl = json.load(f)
+        if token_mapbox is None:
+            token_mapbox = 'pk.eyJ1IjoibWJhcnJlcm9wIiwiYSI6ImNrN28zZjczbTA0ZWwzaXF3aDAxcHl1dGkifQ.DlJqaPP1eC7PF_y-bbWjeg'
+        fig = go.Figure()
+        for j in range(len(y)):
+            fig.add_trace(go.Scattermapbox(mode="markers+lines", lon=x1[0],
+                                           lat=y1[j], line={'color': 'rgba(255, 255, 255, 0.71)'},
+                                           marker={'size': 10, 'color': 'rgba(203, 255, 92, 0.71)'},
+                                           showlegend=False))
+        for i in range(len(x)):
+            fig.add_trace(go.Scattermapbox(mode="markers+lines",
+                                           lon=x1.T[i], lat=y1.T[0],
+                                           line={'color': 'rgba(255, 255, 255, 0.71)'},
+                                           marker={'size': 10, 'color': 'rgba(203, 255, 92, 0.71)'},
+                                           showlegend=False))
+        if c_points:
+            for j in range(len(yp)):
+                fig.add_trace(go.Scattermapbox(mode="markers+lines",
+                                               lon=x2[0], lat=y2[j],
+                                               marker={'size': 10, 'color': 'red'},
+                                               showlegend=False))
+            for i in range(len(xp)):
+                fig.add_trace(go.Scattermapbox(mode="markers+lines",
+                                               lon=x2.T[i], lat=y2.T[0],
+                                               marker={'size': 10, 'color': 'red'},
+                                               showlegend=False))
+
+            fig.update_layout(mapbox={'style': "dark", 'accesstoken': token_mapbox,
+                                      'center': {'lon': -74.081749, 'lat': 4.6097102},
+                                      'zoom': 7, 'layers': [{'source': lcl, 'type': "fill", 'below': "traces",
+                                                             'color': "rgba(92, 214, 255, 0.5)"}]},
+                              margin={'l': 0, 'r': 0, 'b': 0, 't': 0})
+
+        fig.show()
+    return (x, y), (xp, yp)
